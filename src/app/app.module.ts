af28a1eda22a17fa8891a9a58d8cd538a3efe5c1
Cane <ifullstackweb@gmail.com>
2018-06-08 07:36:08 +0200

    Demo Base de Chat en Angular-Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
 
-
+// Componentes
 import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { LoginComponent } from './components/login/login.component';
+
+// Servicios
+import { ChatService} from './providers/chat.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAiC_4IRLi963vW5cOKNw2tv3bx5K3l1h0',
+    authDomain: 'fire-chat-9ab98.firebaseapp.com',
+    databaseURL: 'https://fire-chat-9ab98.firebaseio.com',
+    projectId: 'fire-chat-9ab98',
+    storageBucket: 'fire-chat-9ab98.appspot.com',
+    messagingSenderId: '261045200498'
+  }
 };
